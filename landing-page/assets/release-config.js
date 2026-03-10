c012018622d59165537bd3417fcaff4e64a7a01a
Pawel Komor <pawel@pawelkomor.com>
2026-03-10 15:19:00 +0000

    release: v0.3.8

--- landing-page/assets/release-config.js
+++ landing-page/assets/release-config.js
@@ -1,11 +1,11 @@
 window.__GLANCE_RELEASE__ = {
-  version: 'v0.3.7',
-  windowsLabel: 'v0.3.7 · 64-bit',
-  artifactBaseName: 'Glance_0.3.7',
+  version: 'v0.3.8',
+  windowsLabel: 'v0.3.8 · 64-bit',
+  artifactBaseName: 'Glance_0.3.8',
   workerBaseUrl: 'https://glance-payments.paulus-react.workers.dev',
   downloads: {
-    windows: 'https://github.com/pawelkom88/glance/releases/download/v0.3.7/Glance_0.3.7_x64-setup.exe',
-    macArm: 'https://github.com/pawelkom88/glance/releases/download/v0.3.7/Glance_0.3.7_aarch64.dmg',
-    macIntel: 'https://github.com/pawelkom88/glance/releases/download/v0.3.7/Glance_0.3.7_x64.dmg',
+    windows: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8/Glance_0.3.8_x64-setup.exe',
+    macArm: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8/Glance_0.3.8_aarch64.dmg',
+    macIntel: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8/Glance_0.3.8_x64.dmg',
   },
 };
