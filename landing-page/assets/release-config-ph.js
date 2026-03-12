1e1086d9d666e55fce01ee600fec9ab29b6bb16e
Pawel Komor <pawel@pawelkomor.com>
2026-03-12 19:12:13 +0000

    release: v0.3.8-ph.5 (product hunt)

--- landing-page/assets/release-config-ph.js
+++ landing-page/assets/release-config-ph.js
@@ -1,11 +1,11 @@
 window.__GLANCE_RELEASE_PH__ = {
-  version: 'v0.3.8-ph.4',
-  windowsLabel: 'v0.3.8-ph.4 · Trial · 64-bit',
-  artifactBaseName: 'Glance.Trial_0.3.8-4',
+  version: 'v0.3.8-ph.5',
+  windowsLabel: 'v0.3.8-ph.5 · Trial · 64-bit',
+  artifactBaseName: 'Glance.Trial_0.3.8-5',
   workerBaseUrl: 'https://glance-payments.paulus-react.workers.dev',
   downloads: {
-    windows: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.4/Glance.Trial_0.3.8-4_x64-setup.exe',
-    macArm: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.4/Glance.Trial_0.3.8-4_aarch64.dmg',
-    macIntel: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.4/Glance.Trial_0.3.8-4_x64.dmg',
+    windows: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.5/Glance.Trial_0.3.8-5_x64-setup.exe',
+    macArm: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.5/Glance.Trial_0.3.8-5_aarch64.dmg',
+    macIntel: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.5/Glance.Trial_0.3.8-5_x64.dmg',
   },
 };
