76f422eb5936692db392667c2038814bb546db5f
Pawel Komor <pawel@pawelkomor.com>
2026-03-22 15:22:34 +0000

    landing: point PH Windows download to v0.3.8-ph.8

--- landing-page/assets/release-config-ph.js
+++ landing-page/assets/release-config-ph.js
@@ -1,10 +1,10 @@
 window.__GLANCE_RELEASE_PH__ = {
-  version: 'v0.3.8-ph.6',
-  windowsLabel: 'v0.3.8-ph.6 · Trial · 64-bit',
-  artifactBaseName: 'Glance.Trial_0.3.8-6',
+  version: 'v0.3.8-ph.8',
+  windowsLabel: 'v0.3.8-ph.8 · Trial · 64-bit',
+  artifactBaseName: 'Glance.Trial_0.3.8-8',
   workerBaseUrl: 'https://glance-payments.paulus-react.workers.dev',
   downloads: {
-    windows: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.6/Glance.Trial_0.3.8-6_x64-setup.exe',
+    windows: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.8/Glance.Trial_0.3.8-8_x64-setup.exe',
     macArm: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.6/Glance.Trial_0.3.8-6_aarch64.dmg',
     macIntel: 'https://github.com/pawelkom88/glance/releases/download/v0.3.8-ph.6/Glance.Trial_0.3.8-6_x64.dmg',
   },
